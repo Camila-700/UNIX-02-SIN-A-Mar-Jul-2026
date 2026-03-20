5adbddda895a6b8e2c4afd9c95589fd037d3dcdd
Camila-700 <nalascanoal@uide.edu.ec>
2026-03-20 18:05:27 +0000

    export

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,5 @@ uname -a #Da info del ordenador
 which gpg #ubicación del binario del gpg 
 gpg --version #muestra la versión e info de gpg 
 gpg --full-generate-key #genero la llave publica y privada 
+gpg --list-keys #lista las llaves q hay 
+gpg --armor --export #exporta 
