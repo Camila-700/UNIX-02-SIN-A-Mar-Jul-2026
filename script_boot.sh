eb0709deec3a06404f9da325e133e1ded2e53d08
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-09 12:27:19 +0000

    proyecto folder

---
+++ script_boot.sh
@@ -0,0 +1,10 @@
+git branch 
+#To check the branch you are in
+git checkout 
+#To switch between branches
+mkdir proyecto
+#Creates a new folder named "proyecto" in your current location.
+cd /proyecto
+#Moves you to a folder named "proyecto" located at the root level of the system.
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
+#Navigates directly to that specific folder path.
