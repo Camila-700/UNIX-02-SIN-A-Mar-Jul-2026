c4069bd335f2ceda2f3e35ee9c6200b51ac246cf
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-10 17:15:04 +0000

    Compiling the Linux kernel0

--- script_distro.sh
+++ script_distro.sh
@@ -2,5 +2,21 @@ sudo apt update
 #Updates the package lists on your system so it knows about the latest available software versions.
 sudo apt install -y git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools qemu-
 #Installs multiple development tools and system utilities needed for compiling, building, and running software environments.
+git clone --depth 1 https://github.com/torvalds/linux.git
+#Downloads a shallow copy of the Linux kernel source code repository.
+cd linux
+#Changes the current directory to the downloaded Linux source folder.
+make menuconfig
+#Opens a menu-based interface to configure the kernel options.
+make -j 2
+#Compiles the kernel source code using 2 parallel jobs to speed up the build process.
+sudo mkdir /boot-files
+#Creates a new directory named /boot-files with administrator privileges.
+sudo cp arch/x86/boot/bzImage /boot-files/
+#Copies the compiled kernel image to the /boot-files directory.
+cd ..
+#Moves up one level to the parent directory.
+
+
 
 
