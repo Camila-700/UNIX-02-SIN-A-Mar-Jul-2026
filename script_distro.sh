22b6095380721283bbf197215255d5c20d270a1e
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-10 16:26:30 +0000

    set up facilities

---
+++ script_distro.sh
@@ -0,0 +1,6 @@
+sudo apt update
+#Updates the package lists on your system so it knows about the latest available software versions.
+sudo apt install -y git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools qemu-
+#Installs multiple development tools and system utilities needed for compiling, building, and running software environments.
+
+
