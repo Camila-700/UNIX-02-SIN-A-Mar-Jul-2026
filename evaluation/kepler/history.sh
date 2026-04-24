034f1f3b8c181527b228e55b32ea7c204a24e739
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-24 16:32:10 +0000

    Fix problema 1: estructura de directorios corregida

---
+++ evaluation/kepler/history.sh
@@ -0,0 +1,17 @@
+  1  git fetch upstream
+    2  git checkout -b eval_p2_1_g2 upstream/eval_p2_1_g2
+    3  git push -u origin eval_p2_1_g2
+    4  history
+    5  ls kepler/
+    6  pwd
+    7  ls
+    8  cd kepler
+    9  cd evaluation
+   10  ls
+   11  cd kepler
+   12  ls
+   13  mv docs registros
+   14  mkdir plantillas
+   15  git add .
+   16  git commit -m "Fix problema 1: estructura de directorios corregida"
+   17  history
