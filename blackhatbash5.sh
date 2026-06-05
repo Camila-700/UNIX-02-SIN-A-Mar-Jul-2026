5ac2ea16bf9126cf80b9b410da47811d74977ff7
Camila-700 <nalascanoal@uide.edu.ec>
2026-06-05 17:16:10 +0000

    Add output redirection examples

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -6,3 +6,22 @@ top
 top&
 #intenta ejecutarlo en segundo plano
 
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "EL comando lzl fallo"
+
+echo "Hola Mundo" > output.txt
+cat output.txt
+echo "Chao mundo" > output.txt
+cat output.txt
+echo "Chao chao" >> output.txt
+cat output.txt
+
+ls -l / &> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2>> stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+
+lzl 2> error.txt
+cat error.txt
+
