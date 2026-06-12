00caf27541f7a88d8d6b3dec59b57a0d271a8d9d
Camila-700 <nalascanoal@uide.edu.ec>
2026-06-12 16:38:38 +0000

    Stop file loop script

---
+++ while_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Run this script using Bash
+
+SIGNAL_TO_STOP_FILE="stoploop" 
+# Set the stop signal file name
+
+while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do 
+# Loop while the file does not exist
+  echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..." 
+# Show file not found message
+  echo "Checking again in 2 seconds..." 
+# Show retry message
+  sleep 2 
+# Wait 2 seconds before checking again
+done 
+# End the loop
+
+echo "File was found! Exiting..." 
+# Show exit message
